2b4afcc7fc3ed3d758262d7fd604986ea97455c8
Furkan Nar <narnar660766@gmail.com>
2025-08-04 00:43:45 +0300

    Delete test_redis_connection.py

--- tests/integration/test_redis_connection.py
+++
@@ -1,9 +0,0 @@
-import redis
-
-def test_redis_ping():
-    """
-    Redis sunucusunun ayağa kalktığını ve yanıt verdiğini doğrular.
-    """
-    r = redis.Redis(host="localhost", port=6379, db=0)
-    # Sunucuya ping at ve True döndüğünü assert et
-    assert r.ping() is True
